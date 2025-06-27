1fe85ee97978303cd0c7ff6053ad2132eee09e46
Michał Mrózek <pinguinv2206@gmail.com>
2025-06-27 12:30:28 +0200

    Added light-dark mode toggle

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App {
+  isDarkTheme = false;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    if (this.isDarkTheme) document.documentElement.classList.add('dark');
+    else document.documentElement.classList.remove('dark');
+  }
+}
